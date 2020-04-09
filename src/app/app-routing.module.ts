56da7ff5fa66be0d42c6213586581466e927c881
punter762 <surendra_pratap23@yahoo.in>
2020-04-09 18:43:29 +0530

    converted to single module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,27 @@
-import { NgModule } from "@angular/core";
-import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs/tabs.page';
+import { DashboardPage } from './dashboard/dashboard.page';
+import { AboutPage } from './about/about.page';
+import { ContactPage } from './contact/contact.page';
 
 const routes: Routes = [
   {
-    path: "",
-    loadChildren: () =>
-      import("./tabs/tabs.module").then((m) => m.TabsPageModule),
+    path: '',
+    redirectTo: 'tabs',
+    pathMatch: 'full'
   },
+  {
+    path: 'tabs', component: TabsPage, children:
+    [
+       { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+       { path: 'dashboard', component: DashboardPage},
+       { path: 'about', component: AboutPage},
+       { path: 'contact', component: ContactPage}
+    ]
+  }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
