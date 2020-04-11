5aaff946344f0f20e715969e2e70c947f5f12463
punter762 <surendra_pratap23@yahoo.in>
2020-04-11 17:30:43 +0530

    dashboard

--- src/app/creategroup/creategroup.page.ts
+++ src/app/creategroup/creategroup.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ShareNeedService } from '../share/share-need.service';
 
 @Component({
   selector: 'share-need-next-creategroup',
   templateUrl: './creategroup.page.html',
   styleUrls: ['./creategroup.page.scss'],
 })
-export class CreategroupPage implements OnInit {
+export class CreategroupPage  {
 
-  constructor() { }
+  constructor(public shareNeedService:ShareNeedService) { 
 
-  ngOnInit() {
-  }
+  } 
 
 }
